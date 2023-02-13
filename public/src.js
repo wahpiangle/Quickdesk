66304fbd1e9b767a1ac994d555aed9b18a4be321
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-02-13 16:42:38 +0800

    update

--- public/src.js
+++ public/src.js
@@ -1,6 +1,8 @@
 const switchBtn = document.getElementById('switchBtn');
 const socket = new WebSocket('ws://localhost:3000');
 const offlineButtonArray = document.querySelectorAll('.offline-button');
+const completeCurrentButton = document.querySelector('.complete-current-button');
+const callNextButton = document.querySelector('.call-next-button');
 
 switchBtn.addEventListener('click', () => {
     window.location.href = '/client';
@@ -8,14 +10,14 @@ switchBtn.addEventListener('click', () => {
 
 offlineButtonArray.forEach((button , index) => {
     button.addEventListener('click', (event) => {
-        const counterOfflineTrigger = event.target.dataset.counter;
-        socket.send(counterOfflineTrigger);
         if(button.innerHTML === "Go Offline"){
             button.innerHTML = "Go Online";
             localStorage.setItem(`button-${index}`, "Go Online");
+            socket.send("offline");
         }else if(button.innerHTML === "Go Online"){
             button.innerHTML = "Go Offline";
             localStorage.setItem(`button-${index}`, "Go Offline");
+            socket.send("online");
         }
     });
 });
@@ -26,4 +28,12 @@ window.addEventListener('load', () =>{
             button.innerHTML = storedInnerHTML;
         }
     });
+})
+
+completeCurrentButton.addEventListener('click', () =>{
+    socket.send("completeCurrent");
+});
+
+callNextButton.addEventListener('click', () =>{
+    socket.send("callNext");
 })
